10aec93084db7ea42e12124d3155445e889eaba9
Thail3 <tanai.saeng@gmail.com>
2022-03-06 21:06:08 +0700

    finish static calculator

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import "./App.css";
-import moonIcon from "./assets/moon.png";
-import sunIcom from "./assets/sun.png";
 import Header from "./Component/Header/Header";
 import KeyPad from "./Component/KeyPad/KeyPad";
+import moonIcon from "./assets/moon.png";
+import sunIcom from "./assets/sun.png";
+import "./App.css";
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -19,10 +19,11 @@ function App() {
               className={`app_calculator_navbar_toggle_circle ${
                 isDarkMode ? "app_calculator_navbar_toggle_circle_active" : ""
               }`}
-            ></div>
+            />
           </div>
           <img src={isDarkMode ? moonIcon : sunIcom} alt="mode"></img>
         </div>
+
         <Header />
         <KeyPad />
       </div>
--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -3,8 +3,17 @@ import "./Header.css";
 
 function Header() {
   return (
-    <div className="header">
-      <h1>HEADER</h1>
+    <div className="header custom-scroll">
+      <div className="header_history">
+        <p>10+2</p>
+        <p>10+2</p>
+        <p>10+2</p>
+      </div>
+      <br></br>
+      <div className="header_expression custom-scroll">
+        <p>10+50+89</p>
+      </div>
+      <p className="header_result">149</p>
     </div>
   );
 }
